361aa08a51014e5de8d8b35bdd168d980b2725e9
Throb2003 <161972349+Throb2003@users.noreply.github.com>
2024-04-06 00:51:03 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// script.js
+
+// Event scheduler functionality
+const eventForm = document.getElementById('create-event-form');
+const eventList = document.getElementById('event-list-items');
+
+// Function to create new event item
+function createEventItem(eventName, eventDate) {
+  const eventItem = document.createElement('li');
+  eventItem.innerHTML = `
+    <strong>${eventName}</strong> - ${eventDate}
+  `;
+  return eventItem;
+}
+
+// Function to add event to the list
+function addEventToList(eventItem) {
+  eventList.appendChild(eventItem);
+}
+
+// Event listener for form submission
+eventForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const eventName = document.getElementById('event-name').value;
+  const eventDate = document.getElementById('event-date').value;
+  const newEventItem = createEventItem(eventName, eventDate);
+  addEventToList(newEventItem);
+  eventForm.reset();
+});
 
